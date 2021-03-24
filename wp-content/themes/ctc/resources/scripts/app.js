dbce801dfe595d26990926d3397a4a0e4446e0c6
aidanweltner <aidan.weltner@gmail.com>
2021-03-24 14:00:01 -0600

    Added hamburgers and adjusted menu structure

--- wp-content/themes/ctc/resources/scripts/app.js
+++ wp-content/themes/ctc/resources/scripts/app.js
@@ -6,3 +6,20 @@ import 'jquery';
 $(document).ready(() => {
   // console.log('Hello world');
 });
+
+// Hamburger action
+var burger = document.querySelector('.hamburger');
+var navWrap = document.querySelector('#nav-wrap');
+let menuOpen = false;
+
+burger.addEventListener( 'click', () => {
+  if ( !menuOpen ){
+    burger.classList.add('is-active');
+    navWrap.classList.remove('hidden');
+    menuOpen = true;
+  } else {
+    burger.classList.remove('is-active');
+    navWrap.classList.add('hidden');
+    menuOpen = false;
+  }
+});
--- wp-content/themes/ctc/resources/views/partials/header.blade.php
+++ wp-content/themes/ctc/resources/views/partials/header.blade.php
@@ -1,11 +1,19 @@
-<header class="banner">
+<header class="banner relative flex justify-between items-center">
   <a class="brand" href="{{ home_url('/') }}">
     {{ $siteName }}
   </a>
 
-  <nav class="nav-primary">
-    @if (has_nav_menu('primary_navigation'))
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
-    @endif
-  </nav>
+  <button class="hamburger hamburger--collapse" type="button" aria-label="Menu" aria-controls="navigation">
+    <span class="hamburger-label mr-2">Menu</span>
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+  </button>
+  <div id="nav-wrap" class="hidden absolute -bottom-full right-0">
+    <nav class="nav-primary">
+      @if (has_nav_menu('primary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex space-x-3', 'echo' => false]) !!}
+      @endif
+    </nav>
+  </div>
 </header>
